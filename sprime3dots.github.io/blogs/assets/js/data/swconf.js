a9c2e2e85a843725cf016a037bac9f7253bda579
Tuan Nguyen <nqtuan0192@gmail.com>
2025-04-08 16:11:40 +0900

    Update

---
+++ sprime3dots.github.io/blogs/assets/js/data/swconf.js
@@ -0,0 +1,1 @@
+const swconf={cacheName:"chirpy-1744089180",resources:["/blogs/assets/css/jekyll-theme-chirpy.css","/blogs/","/blogs/categories/","/blogs/tags/","/blogs/archives/","/blogs/about/"],interceptor:{paths:[],urlPrefixes:[]},purge:!1};
