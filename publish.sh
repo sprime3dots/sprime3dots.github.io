08c5ce1bf9eb9eed5cb9db85434af99921a1ba47
Tuan Nguyen <nqtuan0192@gmail.com>
2025-04-08 16:29:08 +0900

    Update

--- publish.sh
+++ publish.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 # set variable for tuannq-nii.github.io
-OUT_DIR="sprime3dots.github.io"
+OUT_DIR="/tmp/sprime3dots.github.io"
 # OUT_DIR="blogs"
 
 # Generate static pages
 bundle exec jekyll build -d $OUT_DIR
+
+rm -rf /tmp/aaa
+mkdir -p /tmp/aaa
+git -C /tmp/aaa clone github_sprime3dots:sprime3dots/sprime3dots.github.io.git
+rsync -avz /tmp/aaa/sprime3dots.github.io/.git $OUT_DIR/
+
 
 # Copy DATA files to OUt_DIR
 cp DATA/CNAME $OUT_DIR
--- sprime3dots.github.io/blogs/sprime3dots.github.io/assets/js/data/swconf.js
+++
@@ -1,1 +0,0 @@
-const swconf={cacheName:"chirpy-1744088973",resources:["/assets/css/jekyll-theme-chirpy.css","/","/categories/","/tags/","/archives/","/about/"],interceptor:{paths:[],urlPrefixes:[]},purge:!1};
